bf4b2f8a9cd1280420136566bfe3d9fd00ee0c6f
SayoWF <149879483+SayoWF@users.noreply.github.com>
2023-12-31 15:19:40 +0900

    Delete javascript_kadai directory

--- javascript_kadai/kadai_020/event.js
+++
@@ -1,8 +0,0 @@
-//btnというidを持つHTML要素を取得し、定数に代入する
-const btn = document.getElementById('btn');
-
-// HTML要素がクリックされたときにイベント処理を実行する
-btn.addEventListener('click', () => {
-    //「ボタンをクリックしてください」を「ボタンをクリックしました」に置き換える
-    document.getElementById('text').innerHTML = 'ボタンをクリックしました';
-});
--- javascript_kadai/kadai_mdn_js/mdn.js
+++
@@ -1,8 +0,0 @@
-//実行する日付を今日で定義する
-const today = new Date();
-const year = today.getFullYear();
-const month = today.getMonth() + 1;
-const date = today.getDate();
-
-//実行する日付を西暦●●●●年●●月●●日と表示する
-console.log(year + '年' + month + '月' + date + '日');
